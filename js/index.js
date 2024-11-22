f2828ac0924fc8baf560a91ffb8cd1ab22fe6225
akmalxonov <117194536+akmalxonov@users.noreply.github.com>
2024-11-23 01:23:16 +0500

    dars32 tailwind css

---
+++ js/index.js
@@ -0,0 +1,24 @@
+tailwind.config = {
+    theme: {
+        container: {
+          center: true,
+          width: {
+            tablet:"682px",
+            desktop:"1170px",
+          },
+          padding: {
+            default:"30px",
+            tablet:"0",
+          },
+        },
+        screens: {
+          tablet:"780px",
+          desktop:"1280px",
+        },
+        extends: {
+          colors:{},
+        }
+
+    },
+  };
+  
